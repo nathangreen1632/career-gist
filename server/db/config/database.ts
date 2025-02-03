351af88703a748fd6f10234b5f8db361e2dd6e14
Star <snabajja@gmail.com>
2025-02-02 20:25:56 -0500

    Build: add code to database.ts

---
+++ server/db/config/database.ts
@@ -0,0 +1,21 @@
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import { Sequelize } from 'sequelize';
+
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+
+const sequelize = process.env.DB_URL
+    ? new Sequelize(process.env.DB_URL)
+    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+export default { sequelize };
